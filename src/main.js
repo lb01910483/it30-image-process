902b5c9f958b1b7912a30bcbfd8e4047e4bdb30b
Henry Chen <lb01910483@gmail.com>
2019-09-14 16:34:43 +0800

    [feat] day 05

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
+import VueGoogleCharts from 'vue-google-charts'
+
+Vue.use(VueGoogleCharts)
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
@@ -12,3 +15,30 @@ new Vue({
   router,
   render: h => h(App)
 })
+
+// options: {
+//   title: 'Approximating Normal Distribution',
+//   legend: { position: 'none' },
+//   colors: ['#4285F4'],
+//   tooltip: {
+//     trigger: 'none'
+//   },
+//   chartArea: { width: 200 },
+//   hAxis: {
+//     ticks: [0, 255]
+//   },
+//   bar: { gap: 0 },
+
+//   histogram: {
+//     bucketSize: 0.02,
+//     minValue: 0,
+//     maxValue: 255
+//   }
+// }
+
+// <GChart
+// v-if="test.length"
+// type="Histogram"
+// :data="test"
+// :options="options"
+// />
